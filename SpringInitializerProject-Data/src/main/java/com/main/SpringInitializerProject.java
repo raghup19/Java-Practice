7bc0e44db7450cad6e669135dd7bdff519274802
Raghu <raghupatil19@gmail.com>
2020-06-19 04:16:43 +0530

    Renamed the class

--- SpringInitializerProject-Data/src/main/java/com/main/SpringInitializerProject.java
+++ SpringInitializerProject-Data/src/main/java/com/main/SpringInitializerProject.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootPracticeSpringInitializerProjectDataApplication {
+public class SpringInitializerProject {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootPracticeSpringInitializerProjectDataApplication.class, args);
+		SpringApplication.run(SpringInitializerProject.class, args);
 	}
 
 }
