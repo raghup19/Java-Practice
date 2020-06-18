f7dce6f9abf6b974eab295050583f549553bca8d
Raghu <raghupatil19@gmail.com>
2020-06-19 04:27:09 +0530

    modified project names

--- SpringInitializerProject-Data/src/test/java/SpringInitializer/SpringInitializerProjectTests.java
+++ SpringInitializerProject-Data/src/test/java/SpringInitializer/SpringInitializerProjectTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootPracticeSpringInitializerProjectDataApplicationTests {
+class SpringInitializerProjectTests {
 
 	@Test
 	void contextLoads() {
